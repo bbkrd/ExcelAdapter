95ed9e4de6a2a0119aa42c7037527ddaf5ee508d
Thomas Witthohn <thomas.witthohn@bundesbank.de>
2017-12-13 10:46:55 +0100

    Extended functionality

--- src/main/java/de/bundesbank/jdemetra/exceladapter/SaveData.java
+++ src/main/java/de/bundesbank/jdemetra/exceladapter/SaveData.java
@@ -5,25 +5,27 @@
  */
 package de.bundesbank.jdemetra.exceladapter;
 
+import ec.satoolkit.DecompositionMode;
 import ec.satoolkit.x11.X11Kernel;
 import ec.tss.Ts;
 import ec.tss.TsFactory;
 import ec.tss.sa.SaItem;
 import ec.tstoolkit.algorithm.CompositeResults;
+import ec.tstoolkit.modelling.ModellingDictionary;
 import ec.tstoolkit.timeseries.simplets.TsData;
 import ec.tstoolkit.timeseries.simplets.TsDataTable;
 import ec.tstoolkit.timeseries.simplets.TsDataTableInfo;
 import ec.tstoolkit.timeseries.simplets.TsDomain;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -34,49 +36,52 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class SaveData {
 
-    private Map<String, List<Ts>> infos = new HashMap<>();
+    private final Map<String, List<Ts>> infos = new TreeMap<>();
+    private static final short DATE_FORMAT = 14;
 
     public void addData(String multiDocName, List<SaItem> items) {
         infos.put(multiDocName, extractInfos(items));
     }
 
     public boolean save() {
-        File myFile = new File("C://Daten/test.xlsx");
-
         XSSFWorkbook workbook = new XSSFWorkbook();
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setDataFormat(DATE_FORMAT);
 
         for (Map.Entry<String, List<Ts>> entry : infos.entrySet()) {
-            XSSFSheet sheet = workbook.createSheet(entry.getKey());
-
             List<Ts> value = entry.getValue();
             TsDataTable dataTable = new TsDataTable();
-
-            Row row = sheet.createRow(0);
-            int columnCount = 0;
-            for (Ts ts : value) {
-                dataTable.insert(-1, ts.getTsData());
-                Cell cell = row.createCell(++columnCount);
-                cell.setCellValue(ts.getRawName());
-            }
+            value.forEach((ts) -> {
+                dataTable.add(ts.getTsData());
+            });
 
             TsDomain dom = dataTable.getDomain();
             if (dom == null || dom.isEmpty()) {
                 //TODO?
+                Logger.getLogger(SaveData.class.getName()).log(Level.INFO, "Nothing in {0}", entry.getKey());
                 continue;
             }
-            // write each rows
+
+            XSSFSheet sheet = workbook.createSheet(entry.getKey());
+            Row header = sheet.createRow(0);
+            int columnCount = 0;
+            for (Ts ts : value) {
+                ++columnCount;
+                Cell cell = header.createCell(columnCount);
+                cell.setCellValue(ts.getRawName());
+            }
+
+            // write each row
             for (int j = 0; j < dom.getLength(); ++j) {
-                Row row2 = sheet.createRow(j + 1);
-                row2.createCell(0).setCellValue(dom.get(j).toString());
+                Row row = sheet.createRow(j + 1);
+                Cell dateCell = row.createCell(0);
+                dateCell.setCellValue(dom.get(j).firstday().getTime());
+                dateCell.setCellStyle(cellStyle);
                 for (int i = 0; i < dataTable.getSeriesCount(); ++i) {
                     TsDataTableInfo dataInfo = dataTable.getDataInfo(j, i);
-                    switch (dataInfo) {
-                        case Valid:
-                            Cell cell = row2.createCell(i + 1);
-                            cell.setCellValue(dataTable.getData(j, i));
-                            break;
-                        default:
-                            break;
+                    if (dataInfo == TsDataTableInfo.Valid) {
+                        Cell cell = row.createCell(i + 1);
+                        cell.setCellValue(dataTable.getData(j, i));
                     }
                 }
             }
@@ -93,14 +98,41 @@ public class SaveData {
 
     private List<Ts> extractInfos(List<SaItem> item) {
         List<Ts> set = new ArrayList<>();
-        item.stream().map((saItem) -> {
+        item.forEach((saItem) -> {
             String name = saItem.getRawName().isEmpty() ? saItem.getTs().getRawName() : saItem.getRawName();
-            CompositeResults compositeResults = saItem.process();
-            TsData d10Data = compositeResults.getData(X11Kernel.D10, TsData.class);
-            Ts d10 = TsFactory.instance.createTs(name + ".d10", null, d10Data);
-            return d10;
-        }).forEachOrdered((d10) -> {
-            set.add(d10);
+            CompositeResults result = saItem.process();
+            boolean isMultiplicative = result.getData(ModellingDictionary.MODE, DecompositionMode.class).isMultiplicative();
+
+            TsData d10 = result.getData(X11Kernel.D10, TsData.class);
+            if (d10 != null) {
+                TsData d10a = result.getData(X11Kernel.D10a, TsData.class);
+                Ts seasonalfactor = TsFactory.instance.createTs(name + ".seasonalfactor", null, d10.update(d10a));
+                set.add(seasonalfactor);
+            }
+
+            TsData calFactorData;
+            TsData a6 = result.getData(X11Kernel.A6, TsData.class);
+            TsData a7 = result.getData(X11Kernel.A7, TsData.class);
+            if (a6 == null) {
+                calFactorData = a7;
+            } else {
+                if (isMultiplicative) {
+                    calFactorData = a6.times(a7);
+                } else {
+                    calFactorData = a6.plus(a7);
+                }
+            }
+            if (calFactorData != null) {
+                Ts calFactor = TsFactory.instance.createTs(name + ".calendarfactor", null, calFactorData);
+                set.add(calFactor);
+            }
+
+            TsData a1 = result.getData(X11Kernel.A1, TsData.class);
+            if (a1 != null) {
+                TsData a1a = result.getData(X11Kernel.A1a, TsData.class);
+                Ts forecast = TsFactory.instance.createTs(name + ".forecast", null, a1.update(a1a));
+                set.add(forecast);
+            }
         });
         return set;
     }
